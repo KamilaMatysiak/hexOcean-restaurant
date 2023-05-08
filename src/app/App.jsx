41e616419a1649ddeda4f43a61d275ec3ba2ebac
xkamikoo <58092037+xkamikoo@users.noreply.github.com>
2023-05-08 14:32:11 +0200

    Initial commit

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import Form from '../components/Form/Form'
+import '../assets/style/main.scss'
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data/index.ts
@@ -0,0 +1,2 @@
+export const dishesTypes = ['pizza', 'soup', 'sandwich']
+export const spicinessScale = [1,2,3,4,5,6,7,8,9,10]
